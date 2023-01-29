5a9bea64c84c0d89f80f523153cfa0b5a71e1602
Vlek <derek@mccammond.org>
2023-01-28 23:44:33 -0800

    Ints to floats, now tests passing.
    At this point, it is working better than expected. Looks like we removed
    the off-by-one issue with the merging because we're not rounding until
    the end. I can do some cleaning, but the algorithm itself works.

--- merger.py
+++ merger.py
@@ -25,7 +25,7 @@ def merge_models(a: str, b: str, alpha: float) -> None:
     print(f"Merging model {a} with model {b} with alpha {alpha * 100}%")
 
 
-def merge(models_and_percentages: dict[str, int]) -> dict[str, int]:
+def merge(models_and_percentages: dict[str, float]) -> dict[str, float]:
     """Merges the models together to come to the same percentages given."""
 
     # We're going to sort in reverse order in the hopes that the smaller
@@ -38,44 +38,41 @@ def merge(models_and_percentages: dict[str, int]) -> dict[str, int]:
         reverse=True,
     )
 
-    merged_models: dict[str, int] = {}
+    merged_models: dict[str, float] = {}
     starting_model: str = sorted_models[0]
 
     # This is going to allow us to know how much of the total we have
     # merged. This is going to inform the amount that we should be
     # adding of any one model.
-    total_merged_percent: int = models_and_percentages[starting_model]
+    total_merged_percent: float = models_and_percentages[starting_model]
 
     # Initializing our mixes with our first model at 100% intensity.
-    merged_models[starting_model] = 100
+    merged_models[starting_model] = 1.0
 
     # For all other models after the first one,
     for index in range(1, len(sorted_models)):
         current_model_name: str = sorted_models[index]
-        current_model_percentage: int = models_and_percentages[current_model_name]
+        current_model_percentage: float = models_and_percentages[current_model_name]
 
-        print(
-            f"Merging model {current_model_name} with intensity {current_model_percentage}"
-        )
+        # print(f"Merging {current_model_name} with intensity {current_model_percentage}")
 
         total_merged_percent += current_model_percentage
-        reduction_in_weights: int = math.floor(
-            current_model_percentage / total_merged_percent * 100
-        )
-        alpha: int = 100 - reduction_in_weights
+        reduction_in_weights: float = current_model_percentage / total_merged_percent
+        alpha: float = 1.0 - reduction_in_weights
 
-        print(f"Total merged at this point: {total_merged_percent}")
-        print(f"This will reduce current weights by {alpha}")
+        # print(f"Reduction in weights: {reduction_in_weights}")
+        # print(f"Total merged at this point: {total_merged_percent}")
+        # print(f"This will reduce current weights by {alpha}")
 
         # Here's where we're going to perform the "merge".
         # What's happening is that we're going to reduce the percentages of all
         # already merged models by the reduction amount. Afterwards, we will
         # add the new model at its intensity.
         for model_name in merged_models:
-            merged_models[model_name] = math.floor(
-                merged_models[model_name] * (alpha / 100)
-            )
+            merged_models[model_name] = round(merged_models[model_name] * alpha, 2)
         merged_models[current_model_name] = reduction_in_weights
+
+        # print(f"Current model weights: {merged_models}")
 
     return merged_models
 
@@ -84,4 +81,4 @@ if __name__ == "__main__":
 
     # merge_models("model a", "model b", 0.90)
 
-    print(merge({"model a": 20, "model b": 30, "model c": 20, "model d": 30}))
+    print(merge({"model a": 0.20, "model b": 0.30, "model c": 0.20, "model d": 0.30}))
